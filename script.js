f6d9366a94a53bf3ff6df2e7de69668fb3a2d139
jaime-rodriguez-olate <53163470+jaime-rodriguez-olate@users.noreply.github.com>
2023-12-11 16:44:24 -0300

    Add files via upload
    Primer entregable.

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Bienvenidos al sistema de monitoreo en línea");
+
+//Me falta crear una funcion.
+
+//variables:
+nombre_usuario = prompt("Por favor indicame tu nombre");
+medicion = prompt("Hola "+ nombre_usuario+". Que quieres medir: 0-Electricidad 1-agua 2-gas 3-temperatura 4-presion 5-agregar otro");
+
+//arrays
+const variablesMedibles = ["electriciad","agua","gas","agregar otro",];
+const datosElectricos = [220,380,440];
+const datosAgua = [1500,1700,2000];
+const datosGas = [100,200,300];
+
+//Bucles:
+if(medicion==5){
+    nuevaVariable = prompt("Ingresa la variable que vas a agregar: ");
+    variablesMedibles.push(nuevaVariable);  
+}
+
+else(true)
+console.log("Ha seleccionado: "+ variablesMedibles[medicion] + ", estos son los datos que hemos capturado");
+
+if(medicion == 0){
+    for(i=0; i< datosElectricos.length;i++)
+            console.log(datosElectricos[i])
+}
+    else if(medicion == 1){
+        for(i=0; i< datosAgua.length;i++)
+            console.log(datosAgua[i])
+    }
+
+    else if(medicion == 2){
+        for(i=0; i< datosGas.length;i++)
+            console.log(datosGas[i])
+    }
+
+    else if(medicion == 3){
+        nueva_variable = []
+        prompt("Has creado una nueva variable, ahora debes agregar datos: ");
+
+    }
